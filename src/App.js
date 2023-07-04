b51669121e4410f166907b4fb58e10016e441689
akshitabajaj03 <akshitabajaj709@gmail.com>
2023-07-04 19:27:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowsweRouter, Routes, Route, BrowserRouter } from 'react-router-dom';
+import Box from '@mui/material/Box';
+
+import { Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './components'
+
+const App=()=> (
+  <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+       <Navbar/>
+       <Routes>
+        <Route exact path="/" element={<Feed/>} />
+        <Route exact path="/video/:id" element={<VideoDetails/>} />
+        <Route exact path="/channel/:id" element={<ChannelDetails/>} />
+        <Route exact path="/search/:searchTerm" element={<SearchFeed/>} />
+       </Routes>
+      </Box>
+  </BrowserRouter>
+);
+
+export default App;
